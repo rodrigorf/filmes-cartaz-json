f1517b54e4d558b2fbfbf4db1a163702e1d72171
Rodrigo Reis Ferreira <rodrigorf33@gmail.com>
2017-03-19 01:41:15 -0300

    Exemplo consultando a lista de filmes em estréia

---
+++ web/flask/estreiasCinema.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from bs4 import BeautifulSoup
+import urllib.request      
+import html
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def summary():
+    html_doc = urllib.request.urlopen("http://www.adorocinema.com/filmes/em-cartaz/estreias/").read()
+    soup = BeautifulSoup(html_doc, "html.parser")
+
+    data = []
+    for dataBox in soup.find_all("div",class_="data_box"):
+        nomeObj = dataBox.find("h2", class_="tt_18 d_inline").find("a", class_="no_underline")
+        imgObj = dataBox.find(class_="img_side_content")
+        sinopseObj = dataBox.find("div", class_="content").find("p")
+        dataObj = dataBox.find("ul", class_="list_item_p2v tab_col_first").find("div", class_="oflow_a")
+
+        data.append({ 'nome': nomeObj.text.strip(),
+                  'poster' : imgObj.img['src'].strip(),
+                  'sinopse' : sinopseObj.text.strip(),
+                  'data' :  dataObj.text.strip()})
+
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+if __name__ == "__main__":
+    app.run()
