14a9543bc0ab69f094d9eb240db55b91c1764333
Rodrigo Reis Ferreira <rodrigorf33@gmail.com>
2017-04-26 21:40:51 -0300

    Correção IP

--- web.py
+++ web.py
@@ -27,4 +27,5 @@ def filmes():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port)
+    # Tem que ser 0.0.0.0 para rodar no Heroku
+    app.run(host='0.0.0.0', port=port)
