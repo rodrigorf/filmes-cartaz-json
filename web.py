3a991ea7c74a52bd1829dd88080d079283a105ef
Rodrigo Reis Ferreira <rodrigorf33@gmail.com>
2017-05-20 01:54:41 -0300

    Porta alterada para 0.0.0.0 para o Heroku

--- web.py
+++ web.py
@@ -30,4 +30,4 @@ if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     # Tem que ser 0.0.0.0 para rodar no Heroku
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
