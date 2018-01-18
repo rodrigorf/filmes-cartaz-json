b1349a1827da332bb7ac2ec37edd05667f3d0c55
rodrigorf <rodrigorf33@gmail.com>
2018-01-18 14:37:55 -0300

    Correção da porta para o Heroku

--- web.py
+++ web.py
@@ -37,6 +37,6 @@ def filmes():
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5200))
+    port = int(os.environ.get('PORT', 5000))
     # Tem que ser 0.0.0.0 para rodar no Heroku
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
