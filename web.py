d3739bbd0124ee6e60570cbe61b27c9b1ddec4e5
rodrigorf <rodrigorf33@gmail.com>
2018-01-18 14:31:54 -0300

    Issue #2 e Adicionado a sinopse completa

--- web.py
+++ web.py
@@ -18,16 +18,25 @@ def filmes():
         imgObj = dataBox.find(class_="thumbnail ")
         sinopseObj = dataBox.find("div", class_="synopsis")
         dataObj = dataBox.find(class_="meta-body").find(class_="meta-body-item meta-body-info")
+        movieLinkObj = dataBox.find(class_="meta-title-link")
+        detailsLink = 'http://www.adorocinema.com' + movieLinkObj.attrs['href']
 
-        data.append({ 'nome': nomeObj.text.strip(),
+        #LOAD FULL SINOPSE 
+        htmldocMovieDetail = urlopen(detailsLink).read()
+        soupMovieDetail = BeautifulSoup(htmldocMovieDetail, "html.parser")
+        fullSinopse = soupMovieDetail.find(class_="synopsis-txt")        
+
+        data.append({   'nome': nomeObj.text.strip(),
                         'poster' : imgObj.img['data-src'].strip(),
                         'sinopse' : sinopseObj.text.strip(),
-                        'data' :  dataObj.text[1:23].strip().replace('/',' ')})
+                        'data' :  dataObj.text[1:23].strip().replace('/',' '),
+                        'link' : detailsLink,
+                        'fullSinopse': fullSinopse.text})
                 
     return jsonify({'filmes': data})
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5200))
     # Tem que ser 0.0.0.0 para rodar no Heroku
     app.run(host='127.0.0.1', port=port)
