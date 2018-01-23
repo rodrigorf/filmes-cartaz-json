89cc812349435fdedb7ce3e3d93a6d62fabdae37
rodrigorf <rodrigorf33@gmail.com>
2018-01-23 00:13:57 -0300

    Erro da porta do Heroku! again!!!

--- web.py
+++ web.py
@@ -67,4 +67,4 @@ if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5200))
     # Tem que ser 0.0.0.0 para rodar no Heroku
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
